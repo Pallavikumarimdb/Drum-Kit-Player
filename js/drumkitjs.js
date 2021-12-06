329a4ad14e81e44eff3183fd1c53d41a3a76a3c1
Pallavi <pallavilpmt1995@gmail.com>
2021-12-06 18:13:50 +0530

    Added animation and keypress method..

--- js/drumkitjs.js
+++ js/drumkitjs.js
@@ -7,26 +7,65 @@ var audio5 = new Audio("sounds/snare.mp3");
 var audio6 = new Audio("sounds/crash.mp3");
 var audio7 = new Audio("sounds/kick-bass.mp3");
 
+for (var i = 0; i < numberOfButtons; i++) {
+  //
+  // document.querySelectorAll(".drum")[i].addEventListener("onKeyPress",function() {
+  //   this.style.color="blue";
+  // })
 
-for (var i = 0; i<numberOfButtons; i++) {
+
+  // FOR MOUSE CLICK EVENT
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    this.style.color = "white";
-    if (this.innerHTML == 'w') {
-      audio1.play();
-    } else if (this.innerHTML == 'a') {
-      audio2.play();
-    } else if (this.innerHTML == 's') {
-      audio3.play();
-    } else if (this.innerHTML == 'd') {
-      audio4.play();
-    } else if (this.innerHTML == 'j') {
-      audio5.play();
-    } else if (this.innerHTML == 'l') {
-      audio6.play();
-    } else if (this.innerHTML == 'k') {
-      audio7.play();
-    } else {
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
 
-    }
+    buttonAnimation(buttonInnerHTML);
+
   });
+}
+
+// FOR BUTTON CLICK EVENT
+document.addEventListener("keypress", function(event) {
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+});
+
+// This is the parametarised function where we can demand for sound response
+// from both mouseclick and keypress
+function makeSound(key) {
+
+//here fpr some reason probably the if-else loop is causing the program to have
+// delay in response with the sounds
+  if (key == 'w') {
+    audio1.play();
+  } else if (key == 'a') {
+    audio2.play();
+  } else if (key == 's') {
+    audio3.play();
+  } else if (key == 'd') {
+    audio4.play();
+  } else if (key == 'j') {
+    audio5.play();
+  } else if (key == 'l') {
+    audio6.play();
+  } else if (key == 'k') {
+    audio7.play();
+  } else {
+
+  }
+}
+
+//This is here for animatons
+function buttonAnimation(keyPressed) {//don't get confsed here keyPressed is just a parameter name
+  var activeButton = document.querySelector("." + keyPressed);
+
+  activeButton.classList.add("pressed");//the pressed class is in csss with
+                                          // fade and taransparency values
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");//adding & removing it with a time
+                                              // slot of 100ms causes an animaton like effect
+  }, 101);
+
 }
